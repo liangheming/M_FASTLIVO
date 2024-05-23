05ff14e336bab28a9438481e50649a3c2fe19b55
liangheming <2605400720@qq.com>
2024-05-23 11:54:43 +0800

    update

--- fastlivo/src/lvio_node.cpp
+++ fastlivo/src/lvio_node.cpp
@@ -3,11 +3,12 @@
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/Image.h>
 #include <tf2_ros/transform_broadcaster.h>
-#include <opencv2/core.hpp>
+
 #include "utils.h"
+#include "map_builder/commons.h"
 #include "map_builder/map_builder.h"
 
-struct Config
+struct NodeConfig
 {
     std::string imu_topic = "/livox/imu";
     std::string lidar_topic = "/livox/lidar";
@@ -15,6 +16,7 @@ struct Config
     std::string map_frame = "lidar";
     std::string body_frame = "body";
 };
+
 struct DataGroup
 {
     bool lidar_pushed = false;
@@ -27,8 +29,8 @@ struct DataGroup
     double last_lidar_time;
     double last_image_time;
 
-    std::deque<livo::IMUData> imu_buffer;
-    std::deque<std::pair<double, livo::CloudType::Ptr>> lidar_buffer;
+    std::deque<IMUData> imu_buffer;
+    std::deque<std::pair<double, CloudType::Ptr>> lidar_buffer;
     std::deque<std::pair<double, cv::Mat>> image_buffer;
 };
 
@@ -40,21 +42,23 @@ public:
         loadCofig();
         initSubsriber();
         initPublisher();
-        m_builder = std::make_shared<livo::MapBuilder>(m_builder_config);
+        m_kf = std::make_shared<IESKF>();
+        m_builder = std::make_shared<MapBuilder>(m_builder_config, m_kf);
         main_loop = m_nh.createTimer(ros::Duration(0.05), &LIVONode::mainCB, this);
     }
 
     void loadCofig()
     {
-        std::vector<double> r_il, p_il, r_cl, p_cl;
-        m_nh.param<std::vector<double>>("r_il", r_il, std::vector<double>());
-        m_nh.param<std::vector<double>>("p_il", p_il, std::vector<double>());
-        m_nh.param<std::vector<double>>("r_cl", r_cl, std::vector<double>());
-        m_nh.param<std::vector<double>>("p_cl", p_cl, std::vector<double>());
+        Vec<double> r_il, t_il, r_cl, t_cl;
+        m_nh.param<Vec<double>>("r_il", r_il, {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0});
+        m_nh.param<Vec<double>>("t_il", t_il, {0.0, 0.0, 0.0});
+        m_nh.param<Vec<double>>("r_cl", r_cl, {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0});
+        m_nh.param<Vec<double>>("t_cl", t_cl, {0.0, 0.0, 0.0});
+    
         m_builder_config.r_il << r_il[0], r_il[1], r_il[2], r_il[3], r_il[4], r_il[5], r_il[6], r_il[7], r_il[8];
-        m_builder_config.p_il << p_il[0], p_il[1], p_il[2];
+        m_builder_config.t_il << t_il[0], t_il[1], t_il[2];
         m_builder_config.r_cl << r_cl[0], r_cl[1], r_cl[2], r_cl[3], r_cl[4], r_cl[5], r_cl[6], r_cl[7], r_cl[8];
-        m_builder_config.p_cl << p_cl[0], p_cl[1], p_cl[2];
+        m_builder_config.t_cl << t_cl[0], t_cl[1], t_cl[2];
         m_builder_config.r_cl = Eigen::Quaterniond(m_builder_config.r_cl).normalized().toRotationMatrix();
     }
 
@@ -85,14 +89,14 @@ public:
         }
 
         m_group_data.last_imu_time = timestamp;
-        m_group_data.imu_buffer.emplace_back(Eigen::Vector3d(msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z) * 10.0,
-                                             Eigen::Vector3d(msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z),
+        m_group_data.imu_buffer.emplace_back(V3D(msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z) * 10.0,
+                                             V3D(msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z),
                                              timestamp);
     }
 
     void lidarCB(const livox_ros_driver::CustomMsg::ConstPtr msg)
     {
-        livo::CloudType::Ptr cloud(new livo::CloudType);
+        CloudType::Ptr cloud(new CloudType);
         livox2pcl(msg, cloud, 3, 0.5);
         std::lock_guard<std::mutex> lock(m_group_data.lidar_mutex);
         double timestamp = msg->header.stamp.toSec();
@@ -127,7 +131,7 @@ public:
         if (!m_group_data.lidar_pushed)
         {
             m_sync_pack.cloud = m_group_data.lidar_buffer.front().second;
-            std::sort(m_sync_pack.cloud->points.begin(), m_sync_pack.cloud->points.end(), [](livo::PointType &p1, livo::PointType &p2)
+            std::sort(m_sync_pack.cloud->points.begin(), m_sync_pack.cloud->points.end(), [](PointType &p1, PointType &p2)
                       { return p1.curvature < p2.curvature; });
             m_sync_pack.cloud_start_time = m_group_data.lidar_buffer.front().first;
             m_sync_pack.cloud_end_time = m_sync_pack.cloud_start_time + m_sync_pack.cloud->points.back().curvature / double(1000.0);
@@ -141,7 +145,7 @@ public:
             if (m_group_data.last_imu_time < m_sync_pack.cloud_end_time)
                 return false;
             double imu_time = m_group_data.imu_buffer.front().timestamp;
-            std::vector<livo::IMUData>().swap(m_sync_pack.imus);
+            Vec<IMUData>().swap(m_sync_pack.imus);
             {
                 std::lock_guard<std::mutex> lock(m_group_data.imu_mutex);
                 while ((!m_group_data.imu_buffer.empty() && (imu_time < m_sync_pack.cloud_end_time)))
@@ -173,7 +177,7 @@ public:
 
         double imu_time = m_group_data.imu_buffer.front().timestamp;
 
-        std::vector<livo::IMUData>().swap(m_sync_pack.imus);
+        Vec<IMUData>().swap(m_sync_pack.imus);
 
         m_sync_pack.image_time = image_start_time;
         m_sync_pack.image = m_group_data.image_buffer.front().second;
@@ -195,7 +199,7 @@ public:
         return true;
     }
 
-    void publishCloud(ros::Publisher &cloud_pub, livo::CloudType::Ptr cloud, std::string &frame_id, double &sec)
+    void publishCloud(ros::Publisher &cloud_pub, CloudType::Ptr cloud, std::string &frame_id, double &sec)
     {
         if (cloud_pub.getNumSubscribers() < 1)
             return;
@@ -223,35 +227,25 @@ public:
         if (!syncPackage())
             return;
 
-        double start_imu_time = 0.0, end_imu_time = 0.0;
-        if (m_sync_pack.imus.size() > 0)
-        {
-            start_imu_time = m_sync_pack.imus.front().timestamp;
-            end_imu_time = m_sync_pack.imus.back().timestamp;
-        }
-        // ROS_WARN("package type: %d, lidar begin: %.4f, imu begin:%.4f, image off: %.4f, imu end: %.4f, lidar end: %4f, imu_size: %lu",
-        //          m_sync_pack.lidar_end,
-        //          m_sync_pack.cloud_start_time,
-        //          start_imu_time,
-        //          m_sync_pack.image_time,
-        //          end_imu_time,
-        //          m_sync_pack.cloud_end_time,
-        //          m_sync_pack.imus.size());
         m_builder->process(m_sync_pack);
 
-        if (m_builder->status() != livo::Status::MAPPING)
+        if (m_builder->status() != BuilderStatus::MAPPING)
             return;
 
         if (m_sync_pack.lidar_end)
         {
-            m_br.sendTransform(eigen2Transform(m_builder->state().rot, m_builder->state().pos, m_node_config.map_frame, m_node_config.body_frame, m_sync_pack.cloud_end_time));
-            publishCloud(m_body_cloud_pub, m_builder->lidar2Body(m_sync_pack.cloud), m_node_config.body_frame, m_sync_pack.cloud_end_time);
-            publishCloud(m_world_cloud_pub, m_builder->lidar2World(m_sync_pack.cloud), m_node_config.map_frame, m_sync_pack.cloud_end_time);
+            m_br.sendTransform(eigen2Transform(m_kf->x().r_wi, m_kf->x().t_wi, m_node_config.map_frame, m_node_config.body_frame, m_sync_pack.cloud_end_time));
+            CloudType::Ptr cloud_body = LidarProcessor::transformCloud(m_sync_pack.cloud, m_kf->x().r_il, m_kf->x().t_il);
+            publishCloud(m_body_cloud_pub, cloud_body, m_node_config.body_frame, m_sync_pack.cloud_end_time);
+            M3D r_wl = m_kf->x().r_wi * m_kf->x().r_il;
+            V3D t_wl = m_kf->x().r_wi * m_kf->x().t_il + m_kf->x().t_wi;
+            CloudType::Ptr cloud_world = LidarProcessor::transformCloud(m_sync_pack.cloud, r_wl, t_wl);
+            publishCloud(m_world_cloud_pub, cloud_world, m_node_config.map_frame, m_sync_pack.cloud_end_time);
         }
         else
         {
-            pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_color = m_builder->lidarSelector()->getCurentCloudRGB();
-            publishCloudColor(m_world_cloud_color_pub, cloud_color, m_sync_pack.image_time);
+            // pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_color = m_builder->lidarSelector()->getCurentCloudRGB();
+            // publishCloudColor(m_world_cloud_color_pub, cloud_color, m_sync_pack.image_time);
         }
     }
 
@@ -267,10 +261,11 @@ private:
 
     ros::Timer main_loop;
     DataGroup m_group_data;
-    Config m_node_config;
-    livo::Config m_builder_config;
-    livo::SyncPackage m_sync_pack;
-    std::shared_ptr<livo::MapBuilder> m_builder;
+    NodeConfig m_node_config;
+    Config m_builder_config;
+    SyncPackage m_sync_pack;
+    std::shared_ptr<IESKF> m_kf;
+    std::shared_ptr<MapBuilder> m_builder;
     tf2_ros::TransformBroadcaster m_br;
 };
 
